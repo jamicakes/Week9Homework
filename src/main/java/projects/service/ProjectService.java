0c0985f5d252ddef6919c153c905c943a6bd3966
Jami <jamitvelez@gmail.com>
2023-05-12 10:40:14 -0700

    added week 9 homework project
    insert project has an error

---
+++ src/main/java/projects/service/ProjectService.java
@@ -0,0 +1,20 @@
+package projects.service;
+
+import projects.dao.ProjectDao;
+import projects.entity.Project;
+
+public class ProjectService {
+
+  private static ProjectDao projectDao = new ProjectDao();
+  
+  
+  
+  
+  public Project addProject(Project project) {
+  return projectDao.insertProject(project); 
+  
+  } // AddProject class - nn work
+
+  } // Project service class 
+
+
